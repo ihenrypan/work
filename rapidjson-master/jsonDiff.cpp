797ef4408c8b8a5b660105b865ef830786bb7f53
ihenrypan <ihenrypan@163.com>
2015-07-10 08:31:28 +0800

    修改：      rapidjson-master/jsonDiff.cpp

--- rapidjson-master/jsonDiff.cpp
+++ rapidjson-master/jsonDiff.cpp
@@ -3,86 +3,97 @@
 #include <cstdio>
 #include <string>
 
-int jsonDiff(FILE* fout, Value* online, Value* offline, int* status, string path)
-{
-    Type onType = online->GetType();
-    Type offType = offline->GetType();
-    
-    if(onType != offType)
-    {
-        *status = 2;
-        return 0;
-    }
-    
-    switch (onType)
-    {
-        case kObjectType:
-        fprintf(fp, "Object:%s\n", path.c_str());
-        string suffix = *(path.end()-1) == '>' ? "" : "->";
-        
-        // iterator online elements
-        Value::ConstMemberIterator it = online->MemberBegin();
-        Value::ConstMemverIterator itEnd = online->MemberEnd();
-        for (; it!=itEnd; it++)
-        {
-            const string key = it->name.GetString();
-            const Value* tmpOnValue = &it->value;
-            const Value* tmpOffValue = &(*offline)[key];    // 用于判断key是否存在
-            
-            if(offline->KeyExist()))
-            {
-                int ret;
-                int status;
-                ret = jsonDiff(fout, tmpOnValue, tmpOffValue, &status, path+suffix);
-                
-                if (ret != 0)
-                {
-                    return ret;
-                }
-                
-                if (status == 2)    // exist both in online && offline
-                {
-                    fprintf(fout, "%s->%s\t%s\t%s\n", path.c_str(), key.c_str(), "online", "offline");
-                }
-                else if(status == 0)    // exist only in online
-                {
-                    fprintf(fout, "%s->%s\t%s\t%s\n", path.c_str(), key.c_str(), "OnlyInOnline", "");
-                }
-                else if (status == 1)   // exist only in offline
-                {
-                    fprintf(fout, "%s->%s\t%s\t%s\n", path.c_str(), key.c_str(), "", "OnlyInOffline");
-                }
-            
-            }
-            else    // exist only in online
-            {
-                fprintf(fout, "%s->%s\t%s\t%s\n", path.c_str(), key.c_str(), "OnlyInOnline", "");
-            }
-            
-        }
-        
-        // iterator offline elements
-        Value::ConstMemberIterator it = offline->MemberBegin();
-        Value::ConstMemverIterator itEnd = offline->MemberEnd();
-        for (; it!=itEnd; it++)
-        {
-            const string key = it->name.GetString();
-            const Value* tmpOffValue = &it->value;
-            const Value* tmpO
-        }
-        
-        break;
-    }
-    if (onType == kObjectType)
-    {
-        fprintf(fp, "Object:%s\n", path.c_str());
-        string suffix = *(path.end()-1) == '>' ? "" : "->";
-        for (Value::ConstMemberIterator it=v->MemberBegin(); it!=v->MemberEnd(); it++)
-            {
-                const string key = it->name.GetString();
-                const Value* tmpValue = &it->value;
-                printJsonTree(fp, (Value *)tmpValue, path + suffix + key);
+using namespace rapidjson;
+using namespace std;
 
-            }
-    }
+int jsonDiff(FILE* fout, Value* onValue, Value* offValue, int* status, string path)
+{
+		Type onType = onValue->GetType();
+		Type offType = offValue->GetType();
+
+		if(onType != offType)
+		{
+				*status = 2;
+				return 0;
+		}
+
+		switch (onType)
+		{
+				case kObjectType:
+						{
+								fprintf(fout, "Object:%s\n", path.c_str());
+								string suffix = "";
+								suffix =  *(path.end()-1) == '>' ? "" : "->";
+
+								// iterator onValue elements
+								Value::ConstMemberIterator it = onValue->MemberBegin();
+								Value::ConstMemberIterator itEnd = onValue->MemberEnd();
+								for (; it!=itEnd; it++)
+								{
+										const char* key = it->name.GetString();
+										Value* tmpOnValue = &it->value;
+										Value* tmpOffValue = &(*offValue)[key];    // 用于判断key是否存在
+
+										if(offValue->KeyExist()) // KeyExist
+										{
+												int ret;
+												int status;
+												ret = jsonDiff(fout, tmpOnValue, tmpOffValue, &status, path+suffix);
+
+												if (ret != 0)
+												{
+														return ret;
+												}
+
+												if (status == 2)    // exist both in onValue && offValue
+												{
+														fprintf(fout, "%s->%s\t%s\t%s\n", path.c_str(), key, "onValue", "offValue");
+												}
+												else if(status == 0)    // exist only in onValue
+												{
+														fprintf(fout, "%s->%s\t%s\t%s\n", path.c_str(), key, "OnlyInOnline", "");
+												}
+												else if (status == 1)   // exist only in offValue
+												{
+														fprintf(fout, "%s->%s\t%s\t%s\n", path.c_str(), key, "", "OnlyInOffline");
+												}
+
+										}
+										else    // exist only in onValue
+										{
+												fprintf(fout, "%s->%s\t%s\t%s\n", path.c_str(), key, "OnlyInOnline", "");
+										}
+
+								}
+
+								// iterator offValue elements
+								it = offValue->MemberBegin();
+								itEnd = offValue->MemberEnd();
+								for (; it!=itEnd; it++)
+								{
+										const char* key = it->name.GetString();
+										const Value* tmpOffValue = &it->value;
+										const Value* tmpOnValue = &(*onValue)[key];
+										if (!onValue->KeyExist())
+										{
+												fprintf(fout, "%s->%s\t%s\t%s\n", path.c_str(), key, "", "OnlyInOffline");
+										}
+								}
+								break;
+						}
+				case kStringType:
+						{
+								// compare two string
+								if (onValue->GetStringLength() != offValue->GetStringLength() || strcmp(onValue->GetString(), offValue->GetString()) != 0 )
+								{
+										*status = 2;
+								}
+								else
+								{
+										*status = 3; // equal	
+								}
+								break;
+						}
+		}
+		return 0;
 }
