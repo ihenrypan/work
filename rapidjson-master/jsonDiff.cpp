89c234988653016a755b2a290d38dd1aff97cc08
ihenrypan <ihenrypan@163.com>
2015-07-12 23:08:45 +0800

    update array diff and gitignore

--- rapidjson-master/jsonDiff.cpp
+++ rapidjson-master/jsonDiff.cpp
@@ -2,98 +2,232 @@
 #include <rapidjson/filereadstream.h>
 #include <cstdio>
 #include <string>
+#include <sstream>
 
 using namespace rapidjson;
 using namespace std;
 
+string ToString(int n)
+{
+    stringstream ss;
+    ss << n;
+    return ss.str();
+}
+
+string GetValueToString(const Value* v)
+{
+    stringstream ss;
+    string ret;
+
+    switch (v->GetType())
+    {
+        case kStringType:
+            ss << v->GetString();
+            break;
+        case kNumberType:
+            if(v->IsInt64())
+            {
+                ss << v->GetInt64();
+            }
+            else
+            {
+                ss << v->GetDouble();
+            }
+            break;
+    }
+    ret = ss.str();
+    
+    return ret;
+}
+
 int jsonDiff(FILE* fout, Value* onValue, Value* offValue, int* status, string path)
 {
-		Type onType = onValue->GetType();
-		Type offType = offValue->GetType();
+    Type onType = onValue->GetType();
+    Type offType = offValue->GetType();
 
-		if(onType != offType)
-		{
-				*status = 2;
-				return 0;
-		}
+    if(onType != offType)
+    {
+        *status = 2;
+        return 0;
+    }
 
-		switch (onType)
-		{
-				case kObjectType:
-						{
-								fprintf(fout, "Object:%s\n", path.c_str());
-								string suffix = "";
-								suffix =  *(path.end()-1) == '>' ? "" : "->";
+    switch (onType)
+    {
+        case kObjectType:
+        {
+            //fprintf(fout, "Object:%s\n", path.c_str());
+            string suffix = "";
+            suffix =  *(path.end()-1) == '>' ? "" : "->";
 
-								// iterator onValue elements
-								Value::ConstMemberIterator it = onValue->MemberBegin();
-								Value::ConstMemberIterator itEnd = onValue->MemberEnd();
-								for (; it!=itEnd; it++)
-								{
-										const char* key = it->name.GetString();
-										Value* tmpOnValue = &it->value;
-										Value* tmpOffValue = &(*offValue)[key];    // 用于判断key是否存在
+            // iterator onValue elements
+            Value::ConstMemberIterator it = onValue->MemberBegin();
+            Value::ConstMemberIterator itEnd = onValue->MemberEnd();
+            for (; it!=itEnd; it++)
+            {
+                const char* key = it->name.GetString();
+                const Value* tmpOnValue = &it->value;
 
-										if(offValue->KeyExist()) // KeyExist
-										{
-												int ret;
-												int status;
-												ret = jsonDiff(fout, tmpOnValue, tmpOffValue, &status, path+suffix);
+				if (offValue->HasMember(key))
+                {
+                    const Value* tmpOffValue = &(*offValue)[key];    
+                    // fprintf(fout, "offValue HasMember(%s)\n", key);
+                    int ret;
+                    int status;
+                    ret = jsonDiff(fout, (Value*)tmpOnValue, (Value*)tmpOffValue, &status, path+suffix+key);
 
-												if (ret != 0)
-												{
-														return ret;
-												}
+                    if (ret != 0)
+                    {
+                        return ret;
+                    }
 
-												if (status == 2)    // exist both in onValue && offValue
-												{
-														fprintf(fout, "%s->%s\t%s\t%s\n", path.c_str(), key, "onValue", "offValue");
-												}
-												else if(status == 0)    // exist only in onValue
-												{
-														fprintf(fout, "%s->%s\t%s\t%s\n", path.c_str(), key, "OnlyInOnline", "");
-												}
-												else if (status == 1)   // exist only in offValue
-												{
-														fprintf(fout, "%s->%s\t%s\t%s\n", path.c_str(), key, "", "OnlyInOffline");
-												}
+                    if (status == 2)    // exist both in onValue && offValue
+                    {
+                        fprintf(fout, "%s->%s|%s|%s\n", path.c_str(), key, GetValueToString(tmpOnValue).c_str(), GetValueToString(tmpOffValue).c_str());
+                    }
+                    else if(status == 0)    // exist only in onValue
+                    {
+                        fprintf(fout, "%s->%s|%s|%s\n", path.c_str(), key, GetValueToString(tmpOnValue).c_str(), "");
+                    }
+                    else if (status == 1)   // exist only in offValue
+                    {
+                        fprintf(fout, "%s->%s|%s|%s\n", path.c_str(), key, "", GetValueToString(tmpOffValue).c_str());
+                    }
 
-										}
-										else    // exist only in onValue
-										{
-												fprintf(fout, "%s->%s\t%s\t%s\n", path.c_str(), key, "OnlyInOnline", "");
-										}
+                }
+                else    // exist only in onValue
+                {
+                    //fprintf(fout, "KeyOnlyInOnline\n");
+                    fprintf(fout, "%s->%s|%s|%s\n", path.c_str(), key, GetValueToString(tmpOnValue).c_str(), "");
+                }
 
-								}
+            }
 
-								// iterator offValue elements
-								it = offValue->MemberBegin();
-								itEnd = offValue->MemberEnd();
-								for (; it!=itEnd; it++)
-								{
-										const char* key = it->name.GetString();
-										const Value* tmpOffValue = &it->value;
-										const Value* tmpOnValue = &(*onValue)[key];
-										if (!onValue->KeyExist())
-										{
-												fprintf(fout, "%s->%s\t%s\t%s\n", path.c_str(), key, "", "OnlyInOffline");
-										}
-								}
-								break;
-						}
-				case kStringType:
-						{
-								// compare two string
-								if (onValue->GetStringLength() != offValue->GetStringLength() || strcmp(onValue->GetString(), offValue->GetString()) != 0 )
-								{
-										*status = 2;
-								}
-								else
-								{
-										*status = 3; // equal	
-								}
-								break;
-						}
-		}
-		return 0;
+            // iterator offValue elements
+            it = offValue->MemberBegin();
+            itEnd = offValue->MemberEnd();
+            for (; it!=itEnd; it++)
+            {
+                const char* key = it->name.GetString();
+                const Value* tmpOffValue = &it->value;
+                if (!onValue->HasMember(key))
+                {
+                    fprintf(fout, "%s->%s|%s|%s\n", path.c_str(), key, "", GetValueToString(tmpOffValue).c_str());
+                }
+            }
+            break;
+        }
+        case kArrayType:
+        {
+            // get array size
+            SizeType onSize = onValue->Size();
+            SizeType offSize = offValue->Size();
+
+            SizeType minSize = onSize < offSize ? onSize : offSize;
+            SizeType i=0;
+            int ret, status;
+            for (; i<minSize; ++i)
+            {
+                Value* tmpOnValue = &(*onValue)[i];
+                Value* tmpOffValue = &(*offValue)[i];
+
+                // itearator every elements..
+                // TODO
+                ret = jsonDiff(fout, tmpOnValue, tmpOffValue, &status, path+"->"+ToString((int)i)); 
+                
+                if (ret != 0)
+                {
+                   return ret; 
+                }
+                if (status == 2)    // exist both in onValue && offValue
+                {
+                    fprintf(fout, "%s|%s|%s\n", path.c_str(), GetValueToString(tmpOnValue).c_str(), GetValueToString(tmpOffValue).c_str());
+                }
+                else if(status == 0)    // exist only in onValue
+                {
+                    fprintf(fout, "%s|%s|%s\n", path.c_str(), GetValueToString(tmpOnValue).c_str(), "");
+                }
+                else if (status == 1)   // exist only in offValue
+                {
+                    fprintf(fout, "%s|%s|%s\n", path.c_str(), "", GetValueToString(tmpOffValue).c_str());
+                }
+
+            }
+            // add extra elements
+            for (; i<onSize; ++i)
+            {
+                Value* tmpOnValue = &(*onValue)[i];
+                fprintf(fout, "%s->%s|%s|%s\n", path.c_str(), ToString((int)i).c_str(), GetValueToString(tmpOnValue).c_str(), "");
+            }
+            for (; i<offSize; ++i)
+            {
+                Value* tmpOffValue = &(*offValue)[i];
+                fprintf(fout, "%s->%s|%s|%s\n", path.c_str(), ToString((int)i).c_str(), "", GetValueToString(tmpOffValue).c_str());
+            }
+
+            break;
+        }
+        case kStringType:
+        {
+            // compare two string
+            if (onValue->GetStringLength() != offValue->GetStringLength() || strcmp(onValue->GetString(), offValue->GetString()) != 0 )
+            {
+                *status = 2;
+            }
+            else
+            {
+                *status = 3; // equal	
+            }
+            break;
+        }
+		case kNumberType:
+        {
+            // compare two number
+			bool isEqual = true;
+			int onIsInt = onValue->IsInt64() ? 1 : 0;
+			int offIsInt = offValue->IsInt64() ? 1 : 0;
+			if( onIsInt == offIsInt)
+			{
+                if((onIsInt = 1 && onValue->GetInt64() == offValue->GetInt64()) || (onValue->GetDouble() == offValue->GetDouble())) {
+                    *status = 3;
+                }
+                else
+                {
+                    *status = 2;
+                }
+			}
+			else
+			{
+                *status = 2;
+			}
+			
+            break;
+        }
+    }
+    return 0;
+}
+
+
+
+int main()
+{
+    FILE* fp = fopen("out.txt", "w+");
+
+    FILE* pFile = fopen("dataA.txt", "rb");
+    char buffer[65536];
+    FileReadStream is(pFile, buffer, sizeof(buffer));
+    Document document;
+    document.ParseStream<0, UTF8<>, FileReadStream>(is);
+
+	FILE* pFile2 = fopen("dataB.txt", "rb");
+    char buffer2[65536];
+    FileReadStream is2(pFile2, buffer2, sizeof(buffer2));
+    Document document2;
+    document2.ParseStream<0, UTF8<>, FileReadStream>(is2);
+
+	int ret, status=0;
+	ret = jsonDiff(fp, &document, &document2, &status, ""); 	
+   
+   	printf("ret: %d, status: %d\n", ret, status);
+
+	return 0;
 }
